71235bb6eb926f7a5790a47e9ede5eca5a7ce2a9
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-23 09:55:49 +0200

    Add files via upload

---
+++ 02.Inheritance-Exercises/01.Person/Child.cs
@@ -0,0 +1,11 @@
+﻿namespace _01.Person
+{
+    public class Child : Person
+    {
+        public Child(string name, int age)
+            : base(name, age)
+        {
+
+        }
+    }
+}
---
+++ 02.Inheritance-Exercises/01.Person/Person.cs
@@ -0,0 +1,22 @@
+﻿namespace _01.Person
+{
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            string result = $"Name: {Name}, Age: {Age}";
+
+            return result.ToString();
+        }
+    }
+}
