6b8cf4d2cfe84c31dc5569e7c55670d6cf5e1494
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-24 00:03:53 +0200

    Add files via upload

---
+++ 08.UnitTesting/04.BankSafe/BankSafe.Tests/BankVaultTests.cs
@@ -0,0 +1,91 @@
+using NUnit.Framework;
+using System;
+
+namespace BankSafe.Tests
+{
+    public class BankVaultTests
+    {
+        private BankVault vault;
+        private Item item;
+
+        [SetUp]
+        public void Setup()
+        {
+            vault = new BankVault();
+            item = new Item("me", "1");
+        }
+
+        [Test]
+        public void WhenCellNotExist()
+        {
+            Exception ex = Assert.Throws<ArgumentException>(() =>
+            {
+                vault.AddItem("go nqma", item);
+
+            });
+
+            Assert.AreEqual(ex.Message, "Cell doesn't exists!");
+        }
+
+        [Test]
+        public void WhenCellIsAlreadyTaken()
+        {
+            Exception ex = Assert.Throws<ArgumentException>(() =>
+            {
+                vault.AddItem("A2", item);
+                vault.AddItem("A2", new Item("Pesho", "2"));
+
+            });
+
+            Assert.AreEqual(ex.Message, "Cell is already taken!");
+        }
+
+        [Test]
+        public void WhenItemAlreadyExist()
+        {
+            Exception ex = Assert.Throws<InvalidOperationException>(() =>
+            {
+                vault.AddItem("A2", item);
+                vault.AddItem("B2", item);
+
+            });
+
+            Assert.AreEqual(ex.Message, "Item is already in cell!");
+        }
+
+        [Test]
+        public void WhenRemoveAndCellNotExist()
+        {
+            Exception ex = Assert.Throws<ArgumentException>(() =>
+            {
+                vault.RemoveItem("go nqma", item);
+
+            });
+
+            Assert.AreEqual(ex.Message, "Cell doesn't exists!");
+        }
+
+        [Test]
+        public void WhenRemoveAndItemNotExist()
+        {
+            Exception ex = Assert.Throws<ArgumentException>(() =>
+            {
+                vault.RemoveItem("A2", item);
+
+            });
+
+            Assert.AreEqual(ex.Message, "Item in that cell doesn't exists!");
+        }
+
+        //[Test]
+        //public void WhenRemoveAndItemInThatCellNotExist()
+        //{
+        //    Exception ex = Assert.Throws<ArgumentException>(() =>
+        //    {
+        //        vault.AddItem("A2", item);
+        //    });
+
+        //    Assert.AreEqual(ex.Message, "Item in that cell doesn't exists!");
+        //}
+    }
+}
