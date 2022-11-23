e0db3e8cac4f6f88902ab67b2397bba0d376f1e3
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-23 10:58:27 +0200

    Add files via upload

---
+++ 04.Encapsulation-Exercises/03.ShoppingSpree/Product.cs
@@ -0,0 +1,40 @@
+﻿namespace _03.ShoppingSpree
+{
+    public class Product
+    {
+        private string name;
+        private decimal cost;
+
+        public Product(string name, decimal cost)
+        {
+            Name = name;
+            Cost = cost;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (value.Length == 0)
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                this.name = value;
+            }
+        }
+
+        public decimal Cost
+        {
+            get => this.cost;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Cost cannot be negative");
+                }
+                this.cost = value;
+            }
+        }
+    }
+}
