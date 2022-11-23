52641af50641030535a8b489bdb4b96d23579c19
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-23 10:02:52 +0200

    Add files via upload

---
+++ 06.Animals/Cat.cs
@@ -0,0 +1,16 @@
+﻿namespace _06.Animals
+{
+    public class Cat : Animals
+    {
+        public Cat(string name, int age, string gender)
+            : base(name, age, gender)
+        {
+
+        }
+
+        public override string ProduceSound()
+        {
+            return "Meow meow";
+        }
+    }
+}
