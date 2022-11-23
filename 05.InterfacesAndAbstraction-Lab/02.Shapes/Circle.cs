2cfc8278cb905b6e8ff66d11f9a1d342738280c1
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-23 23:24:50 +0200

    Add files via upload

---
+++ 05.InterfacesAndAbstraction-Lab/02.Shapes/Circle.cs
@@ -0,0 +1,44 @@
+﻿namespace _02.Shapes
+{
+    public class Circle : IDrawable
+    {
+        private int radius;
+
+        public Circle(int radius)
+        {
+            Radius = radius;
+        }
+
+        public int Radius
+        {
+            get { return this.radius; }
+            set { this.radius = value; }
+        }
+
+        public void Draw()
+        {
+            double rin = this.radius - 0.4;
+            double rOut = this.radius + 0.4;
+
+            for (int y = this.radius; y >= -this.radius; --y)
+            {
+                for (double x = -this.radius; x < rOut; x += 0.5)
+                {
+                    double value = x * x + y * y;
+
+                    if (value >= rin * rin && value <= rOut * rOut)
+                    {
+                        Console.WriteLine("*");
+                    }
+                    else
+                    {
+                        Console.WriteLine(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
---
+++ 05.InterfacesAndAbstraction-Lab/02.Shapes/IDrawable.cs
@@ -0,0 +1,7 @@
+﻿namespace _02.Shapes
+{
+    public interface IDrawable
+    {
+        void Draw();
+    }
+}
