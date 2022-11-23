444207cef1349157aa2db823b1da18707a51a073
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-23 10:01:54 +0200

    Add files via upload

---
+++ 04.NeedForSpeed/FamilyCar.cs
@@ -0,0 +1,11 @@
+﻿namespace _04.NeedForSpeed
+{
+    public class FamilyCar : Car
+    {
+        public FamilyCar(int horsePower, double fuel)
+            : base(horsePower, fuel)
+        {
+
+        }
+    }
+}
---
+++ 04.NeedForSpeed/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿namespace _04.NeedForSpeed
+{
+    public class Vehicle
+    {
+        private const double DefaultFuelConsumption = 1.25;
+
+        public Vehicle(int horsePower, double fuel)
+        {
+            HorsePower = horsePower;
+            Fuel = fuel;
+        }
+
+        public int HorsePower { get; set; }
+
+        public double Fuel { get; set; }
+
+        public virtual double FuelConsumption => DefaultFuelConsumption;
+
+        public virtual void Drive(double kilometers)
+        {
+            double sum = kilometers * this.FuelConsumption;
+
+            this.Fuel -= sum;
+        }
+    }
+}
