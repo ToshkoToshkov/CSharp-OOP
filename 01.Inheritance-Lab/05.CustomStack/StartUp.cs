2ee3a469848bf166f87245e0dba0df11a237279c
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-23 09:51:44 +0200

    Add files via upload

---
+++ 01.Inheritance-Lab/05.CustomStack/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace _05.CustomStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stack = new StackOfStrings();
+
+            stack.AddRange(new List<string>()
+            {
+                "123",
+                "456",
+                "789",
+                "gogi"
+            });
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
