ab0cab71e204666b803af8d1cce346c320849257
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-23 23:58:10 +0200

    Add files via upload

---
+++ 08.UnitTesting/03.Presents/Presents.tests/PresentsTests.cs
@@ -0,0 +1,60 @@
+﻿namespace Presents.Tests
+{
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+
+    [TestFixture]
+    public class PresentsTests
+    {
+        private Bag bag;
+        private Present present;
+        private List<Present> list;
+
+        [SetUp]
+        public void SetUp()
+        {
+            bag = new Bag();
+            present = new Present("Pesho", 2);
+            list = new List<Present>();
+        }
+
+        [Test]
+        public void TryCreateWhenPresentIsNUll()
+        {
+            Assert.Throws<ArgumentNullException>(() => this.bag.Create(null));
+        }
+
+        [Test]
+        public void TryCreateWhenAlreadyExist()
+        {
+            bag.Create(present);
+            Assert.Throws<InvalidOperationException>(() => this.bag.Create(present));
+        }
+
+        [Test]
+        public void TryRemovePresent()
+        {
+            list.Add(present);
+            list.Remove(present);
+            Assert.That(list.Count, Is.Zero);
+        }
+
+        [Test]
+        public void IncreaseWhenAddPresent()
+        {
+            this.list.Add(present);
+            Assert.That(list.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void getPresent()
+        {
+            Present pres = new Present("Ico", 5);
+            bag.Create(pres);
+            var something = bag.GetPresentWithLeastMagic();
+
+            Assert.That(something, Is.EqualTo(pres));
+        }
+    }
+}
