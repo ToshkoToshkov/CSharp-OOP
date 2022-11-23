25916f1882651892da31e512209b2d61468f3796
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-23 10:05:59 +0200

    Add files via upload

---
+++ 02.Inheritance-Exercises/05.Restaurant/Coffee.cs
@@ -0,0 +1,13 @@
+﻿namespace _05.Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        public Coffee(string name, double caffeine)
+            : base(name, 3.5M, 50)
+        {
+            Caffeine = caffeine;
+        }
+
+        public double Caffeine { get; set; }
+    }
+}
