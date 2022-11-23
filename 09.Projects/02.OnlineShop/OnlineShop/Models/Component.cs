924045157de89da3392999730a35b0c3c717fec8
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-23 23:41:47 +0200

    Add files via upload

---
+++ 09.Projects/02.OnlineShop/OnlineShop/Models/Component.cs
@@ -0,0 +1,22 @@
+﻿using OnlineShop.Models.Products.Components;
+
+namespace OnlineShop.Models
+{
+    public abstract class Component : Product, IComponent
+    {
+        public Component(int id, string manufacturer, string model, decimal price, double overallPerformance, int generation)
+            : base(id, manufacturer, model, price, overallPerformance)
+        {
+            Generation = generation;
+        }
+
+        public int Generation { get; private set; }
+
+        public override string ToString()
+        {
+            return $"Overall Performance: {this.OverallPerformance}. Price: {this.Price} - {this.GetType().Name}: {this.Manufacturer} {this.Model} (Id: {this.Id}) Generation: {this.Generation}";
+        }
+    }
+
+    
+}
---
+++ 09.Projects/02.OnlineShop/OnlineShop/Models/Computer.cs
@@ -0,0 +1,64 @@
+﻿using OnlineShop.Models.Products.Components;
+using OnlineShop.Models.Products.Computers;
+using OnlineShop.Models.Products.Peripherals;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShop.Models
+{
+    public abstract class Computer : Product, IComputer
+    {
+        private IReadOnlyCollection<IComponent> components;
+        private IReadOnlyCollection<IPeripheral> peripherals;
+
+
+        public Computer(int id, string manufacturer, string model, decimal price, double overallPerformance)
+            : base(id, manufacturer, model, price, overallPerformance)
+        {
+
+        }
+
+        public IReadOnlyCollection<IComponent> Components { get; private set; }
+
+        public IReadOnlyCollection<IPeripheral> Peripherals { get; private set; }
+
+        public void AddComponent(IComponent component)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddPeripheral(IPeripheral peripheral)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IComponent RemoveComponent(string componentType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPeripheral RemovePeripheral(string peripheralType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.AppendLine($"Overall Performance: {this.OverallPerformance}. Price: {this.Price} - {this.GetType().Name}: {this.Manufacturer} {this.Model} (Id: {this.Id})");
+
+            result.AppendLine($" Components ({components.Count}):");
+
+            foreach (var item in components)
+            {
+                result.AppendLine($"{item.ToString()}");
+            }
+
+            result.AppendLine($" Peripherals ({peripherals.Count}); Average Overall Performance ({this.OverallPerformance}):");
+
+            return result.ToString();
+        }
+    }
+}
