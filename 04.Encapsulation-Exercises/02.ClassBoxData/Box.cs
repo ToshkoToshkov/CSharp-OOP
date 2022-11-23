d9d52d96037a26853a7c795bba597c515879ebb7
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-23 10:56:51 +0200

    Add files via upload

---
+++ 04.Encapsulation-Exercises/02.ClassBoxData/Box.cs
@@ -0,0 +1,73 @@
+﻿namespace _02.ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length
+        {
+            get => this.length;
+            private set
+            {
+                this.ThrowIfInvalidSaid(value, nameof(Length));
+
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => this.width;
+            private set
+            {
+                this.ThrowIfInvalidSaid(value, nameof(Width));
+
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get => this.height;
+            private set
+            {
+                this.ThrowIfInvalidSaid(value, nameof(Height));
+
+                this.height = value;
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            return 2 * this.length * this.width + 2 * this.length * this.height + 2 * this.width * this.height;
+        }
+
+        public double LateralSurfaceArea()
+        {
+            return 2 * this.length * this.height + 2 * this.width * this.height;
+        }
+
+        public double Volume()
+        {
+            return this.length * this.width * this.height;
+        }
+
+
+        private void ThrowIfInvalidSaid(double value, string side)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"{side} cannot be zero or negative.");
+            }
+        }
+    }
+}
