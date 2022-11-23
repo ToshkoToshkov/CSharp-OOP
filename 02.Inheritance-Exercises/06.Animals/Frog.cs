786dada01f0b9d20ad9252f80860338a0a60dfb3
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-23 10:06:25 +0200

    Add files via upload

---
+++ 02.Inheritance-Exercises/06.Animals/Frog.cs
@@ -0,0 +1,16 @@
+﻿namespace _06.Animals
+{
+    public class Frog : Animals
+    {
+        public Frog(string name, int age, string gender)
+            : base(name, age, gender)
+        {
+
+        }
+
+        public override string ProduceSound()
+        {
+            return "Ribbit";
+        }
+    }
+}
